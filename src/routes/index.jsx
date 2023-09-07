ad896dae6d78abeb6939e2513cd48d3cac3103f5
Marco Aurelio Batista <marco.batista.pb@compasso.com.br>
2023-09-07 02:09:41 -0300

    Home page creation

---
+++ src/routes/index.jsx
@@ -0,0 +1,8 @@
+import { Route, Routes } from 'react-router-dom'
+import Home from '../pages/Home'
+
+export default function RoutesApp() {
+    <Routes>
+        <Route path={'/'} element={<Home />} />
+    </Routes>
+}
