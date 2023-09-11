c9bc48ec3a77796d0a226b567135be05c632b627
Marco Aurelio Batista <marco.batista.pb@compasso.com.br>
2023-09-11 00:26:53 -0300

    Header Finish

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,14 +7,14 @@ export default function Header({ mode, setMode }) {
 
     return (
         <>
-            <div className={styles.headerContainer}>
+            <div className={`${styles.headerContainer} ${mode ? 'bg-light' : 'bg-dark'}`}>
                 <div className={styles.header}>
                     <span className={`${styles.logo} ${mode ? 'text-primary-light' : 'text-primary-dark'}`}>Marco Aurélio</span>
                     <div>
-                        <span className={mode ? 'text-primary-light' : 'text-primary-dark'}>About</span>
-                        <span className={mode ? 'text-primary-light' : 'text-primary-dark'}>Projects</span>
-                        <span className={mode ? 'text-primary-light' : 'text-primary-dark'}>Abilities</span>
-                        <span className={mode ? 'text-primary-light' : 'text-primary-dark'}>Contact</span>
+                        <span className={mode ? 'text-primary-light text-underline-light' : 'text-primary-dark text-underline-dark'}>About</span>
+                        <span className={mode ? 'text-primary-light text-underline-light' : 'text-primary-dark text-underline-dark'}>Projects</span>
+                        <span className={mode ? 'text-primary-light text-underline-light' : 'text-primary-dark text-underline-dark'}>Abilities</span>
+                        <span className={mode ? 'text-primary-light text-underline-light' : 'text-primary-dark text-underline-dark'}>Contact</span>
                         {mode ? (
                             <FiSun size={25} onClick={() => setMode(!mode)} className={mode ? 'text-primary-light' : 'text-primary-dark'} />
                         ) : (
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,7 @@
 import styles from './styles.module.scss'
 import Header from '../../components/Header'
 import { useEffect, useState } from 'react'
+import profilePic from '../../assets/pics/picture.png'
 
 export default function Home() {
     const [lightMode, setLightMode] = useState(false)
@@ -8,8 +9,12 @@ export default function Home() {
     return (
         <div className={`page ${lightMode ? 'bg-light' : 'bg-dark'}`}>
             <Header mode={lightMode} setMode={setLightMode} />
-            <section>
-                <span className={lightMode ? 'text-dark' : 'text-light'}>página hooooomeeeee</span>
+            <section className={styles.firstSection}>
+                <div className={styles.picture}></div>
+                <span className={`${styles.title} ${lightMode ? 'text-dark' : 'text-light'}`}>
+                    Hi! My name is <span style={{ fontFamily: 'Imbue, serif' }}>Marco Aurélio </span>
+                    and I'm a React Front-End developer.
+                </span>
             </section>
         </div>
     )
